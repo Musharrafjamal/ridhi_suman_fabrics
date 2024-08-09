fad18057a896ff47c404bb66768d1f651eb379e2
Gautam Raj <ashragautam25@gmail.com>
2024-08-10 03:11:47 +0530

    chore: Disable option to pending orders in AdminOrders page and update cancel order API route

--- app/api/admin/shiprocket/cancel-order/route.js
+++ app/api/admin/shiprocket/cancel-order/route.js
@@ -1,10 +1,12 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { checkAuthorization } from "@/config/checkAuthorization";
 
 export async function POST(req) {
-  const email = process.env.SHIPROCKET_EMAIL;
-  const password = process.env.SHIPROCKET_PASSWORD;
+  const headerList = headers();
+
+  const token = headerList.get("authorization");
 
   try {
     const isAdmin = await checkAuthorization(req);
@@ -13,23 +15,24 @@ export async function POST(req) {
       return NextResponse.json("Unauthorized Request", { status: 401 });
     }
 
+    const body = await req.json();
+
     const res = await fetch(
-      "https://apiv2.shiprocket.in/v1/external/auth/login",
+      "https://apiv2.shiprocket.in/v1/external/orders/cancel",
       {
         method: "POST",
+        redirect: "follow",
         headers: {
           "Content-Type": "application/json",
+          Authorization: token,
         },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
+        body: JSON.stringify(body),
       }
     );
 
-    const shiprocketToken = await res.json();
+    const shiprocketInfo = await res.json();
 
-    return NextResponse.json(shiprocketToken, { status: 200 });
+    return NextResponse.json(shiprocketInfo, { status: 200 });
   } catch (err) {
     return NextResponse.json(err, { status: 500 });
   }
